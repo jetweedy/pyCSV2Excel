1da09327ef47a7434d41cd3494a24d781e14c26a
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-02-24 10:14:25 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # pyCSV2Excel
-
-## Command Line Usage
-
-```
- python3 csv2xlsx.py targetfile.xlsx sourcefile1 sourcefile2 etc
-```
--- csv2xlsx.py
+++ csv2xlsx.py
@@ -5,30 +5,38 @@
 import os
 import csv
 import sys
-import glob    
+import glob
+import time;
+
+ts1 = time.time();
 
 import pandas as pd
 from pandas.io.excel import ExcelWriter
 from openpyxl import load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument List:', str(sys.argv))
 
 if (len(sys.argv) > 1):
 
 	all_files = sys.argv;
 	all_files.pop(0);
+	targetFile = all_files.pop(0);
 
 	#all_files = ["csv1.csv", "csv5.csv"];
 	#all_files = ["csv3.csv", "csv4.csv"];
 
-	df_from_each_file = (pd.read_csv(f) for f in all_files)
+	df_from_each_file = (pd.read_csv(f, low_memory=False) for f in all_files)
 
-	writer = pd.ExcelWriter("result.xlsx", engine='xlsxwriter');
+	writer = pd.ExcelWriter(targetFile, engine='xlsxwriter');
 	for idx, df in enumerate(df_from_each_file):
 		df.to_excel(writer, sheet_name='Sheet {0}'.format(idx), index=False, encoding='utf-8')
 	writer.save()
+
+
+ts2 = time.time();
+print("\nExecution time: ", ts2-ts1);
 
 
 ### Below is code demonstrating how to do some more dynamic cell creation
